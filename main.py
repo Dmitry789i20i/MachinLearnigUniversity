7d5cc0ab224fd71867708821c9c32b04e24ecc6c
dmitry <pavlov.dmitru789i@gmail.com>
2025-09-26 22:33:39 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd # Для работы с данными
+import scipy.stats # При работе со статистикой
+import matplotlib.pyplot as plt  # Библиотека для визуализации результатов 
+
+
